82354a9a9bc93b2ff504cf788f250d9f1b91d7ea
Ryan Collingwood <ryancollingwood@gmail.com>
2019-04-27 11:51:32 +1000

    getting deeper into domainIncludes and rangeIncludes for properties

--- onto_adapter.py
+++ onto_adapter.py
@@ -84,34 +84,52 @@ class OntoAdapter(LogicAdapter):
 
         import random
 
-        # Randomly select a confidence between 0 and 1
+        # DEBUG Randomly select a confidence between 0 and 1
         confidence = uniform(0, 1)
 
         parsed = self.parse_input(input_statement.text)
 
         object_uri = URIRef("http://schema.org/{}".format(parsed["object"]))
         domain_includes_uri = URIRef("http://schema.org/domainIncludes")
+        range_includes_uri = URIRef("http://schema.org/rangeIncludes")
 
-        # self.load_schema_for_type(parsed["object"])
-        # property = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#Property")
-        print("object_uri", object_uri)
+        print("object_uri", object_uri)   
+
+        chosen_object_subject = None
 
         for subject in self.ontology.subjects(domain_includes_uri, object_uri):
             subject_uri = URIRef(subject)
-
             subject_label = (list(self.ontology.preferredLabel(subject_uri)))
+            if subject_label and len(subject_label) > 1:
+                chosen_object_subject = subject_label[1]
             subject_comment = self.ontology.comment(subject_uri)
-            print("\t", subject, subject_label, subject_comment)
 
-        try:
-            # print_values("domainIncludes", self.ontology.subjects(domain_includes_uri, object_uri))
-            pass
-        except:
-            # eat it for now
-            pass
+            # for input: "Avengers is a Move"
+            # subject_label = [(rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#label'), rdflib.term.Literal('musicBy'))]
+            print("\t", subject_label, subject_label, subject_comment)
 
+            # for each of the subjects print out the expected types of the data
+            for range_type in self.ontology.objects(subject_uri, range_includes_uri):
+                range_uri = URIRef(range_type)
+                range_label = (list(self.ontology.preferredLabel(range_uri)))
+                range_comment = self.ontology.comment(range_uri)            
+                print("\t\t", range_type, range_label, range_comment)                
+                
         # For this example, we will just return the input as output
-        selected_statement = Statement(text = "What is {}?".format(parsed["instance"]))
-        selected_statement.confidence = confidence
-
+        if (subject_label):
+            selected_statement = Statement(
+                text = "What is {subject_label} for {instance}?".format(
+                    subject_label = subject_label,
+                    instance = parsed["instance"]
+                    )     
+                )
+            selected_statement.confidence = confidence
+        else:
+            selected_statement =  Statement(
+                text = "I'm sorry I don't know what sort of thing a '{instance}' is.".format(
+                    instance = parsed["instance"]
+                    )
+                )
+            selected_statement.confidence = 0
+            
         return selected_statement
